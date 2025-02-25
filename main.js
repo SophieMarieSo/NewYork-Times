5de6aa9da90f18b04527323fdccf3627f63e3361
AhnEunJin <eunjin.an@datastreams.co.kr>
2025-02-25 23:51:43 +0900

    noona's data api

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ let news = [];
 
 const getLatestNews = async () => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
     );
     const resp = await fetch(url);
     const data = await resp.json();
     news = data.articles;
+
+    console.log(news);
 };
 
 getLatestNews();
