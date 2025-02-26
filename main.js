237e71ee4ab409075141ad902ad73cb5ff21beaa
AhnEunJin <eunjin.an@datastreams.co.kr>
2025-02-26 23:30:49 +0900

    search form

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const API_KEY = '102e645856ae42da864a934bfb72a4a0';
-let news = [];
+// const API_KEY = '102e645856ae42da864a934bfb72a4a0';
+let newsList = [];
+const searchBtn = document.getElementById('search-btn');
 
 const getLatestNews = async () => {
     const url = new URL(
@@ -8,9 +9,36 @@ const getLatestNews = async () => {
     );
     const resp = await fetch(url);
     const data = await resp.json();
-    news = data.articles;
+    newsList = data.articles;
 
-    console.log(news);
+    render();
+    console.log(newsList);
 };
+
+const render = () => {
+    let newsHtml = ``;
+
+    newsHtml = newsList.map(
+        (news) => `<div class="row news pt-3 pb-3">
+                    <div class="col-lg-4 col-12 pb-2">
+                        <img
+                            class="img-thumbnail rounded img-fluid news-img "
+                            src=${news.urlToImage}
+                        />
+                    </div>
+                    <div class="col-lg-8 col-12">
+                        <h3 class="pb-2">${news.title}</h3>
+                        <p>${news.description}</p>
+                        <div>${news.source.name} ${news.publishedAt}</div>
+                    </div>
+                </div>`
+    );
+
+    document.getElementById('news-board').innerHTML = newsHtml.join('');
+};
+
+searchBtn.addEventListener('click', () =>
+    document.getElementById('search-field').classList.toggle('active')
+);
 
 getLatestNews();
